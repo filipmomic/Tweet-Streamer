5d44b97d777d07de1d45b6bb6ed7062da4805607
filipmomic <71992827+filipmomic@users.noreply.github.com>
2022-04-18 20:28:40 -0500

    Update on_data method to write to dynamo db
    Added connect_target_table to connect to dynamo db.
    
    Modified TweetListener.on_data method to write to dynamo db.

--- tweetstreamer.py
+++ tweetstreamer.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import tweepy
 import config
+import boto3
 
 load_dotenv()
 
@@ -14,22 +15,39 @@ def get_secret(secretName):
 class TweetListener(tweepy.StreamingClient):
     #Inherits from tweepy.StreamingClient class
     
-    def __init__(self, max_tweets, *args, **kwargs):
+    def __init__(self, target_table, max_tweets, *args, **kwargs):
         # Initialize super class - tweepy.StreamingClient 
         super().__init__(*args, **kwargs)
         # Initialize sub class - TweetListener
         self.max_tweets = max_tweets
         self.tweets_count = 0
+        self.table = target_table
     
     def on_connect(self):
         # Method called when connected
         print("Listening for tweets..")
         
     def on_data(self, raw_data):
-        # Method called when data is returned by stream
+        # Method called when tweet is returned by stream
         self.tweets_count += 1
-        print(raw_data)
+        json_data = json.loads(raw_data)
         
+        #print(json_data) #testing
+        
+        content = {}
+        content['tweet_id'] = int(json_data['data']['id']) #table key
+        content['created_at'] = json_data['data']['created_at']
+        content['text'] = json_data['data']['text']
+
+        #print(content['text'] + '\n') #testing
+        
+        try:
+            self.table.put_item(Item=content)
+            print(f"Wrote {self.tweets_count} tweets to dynamo successfully!")
+        except Exception as e:
+            print(str(e))
+        
+        #limit streaming during development/testing
         if self.tweets_count == self.max_tweets:
             print("Closing the connection.. reached max_tweets limit: ", self.max_tweets)
             self.disconnect()
@@ -41,6 +59,18 @@ class TweetListener(tweepy.StreamingClient):
     def on_response(self, response):
         print("Recieved response:", str(response))
 
+def connect_target_table(tableName):
+    aws_access_id = get_secret('ACCESSKEYID')
+    aws_secret_access_key = get_secret('SECRETKEY')
+    
+    session = boto3.Session(region_name='us-west-1',
+                        aws_access_key_id=aws_access_id,
+                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    ddb = session.resource('dynamodb')
+    table = ddb.Table(tableName)
+    
+    return table
+ 
 def create_stream_rules(objList, lang):
     # Create a list of tweepy.StreamRules objects to pass into StreamingClient
     
@@ -49,11 +79,14 @@ def create_stream_rules(objList, lang):
         ruleStr = obj + ' lang:' + lang
         streamRuleList.append(tweepy.StreamRule(value=ruleStr))
         
-    return streamRuleList
+    return streamRuleList   
 
 if __name__ == '__main__':
+    #db connection
+    dynamo_table = connect_target_table('tweetstreamer')
+    
     secret = get_secret('BEARERTOKEN')
-    client = TweetListener(bearer_token=secret, max_retries=3, max_tweets=20)
+    client = TweetListener(bearer_token=secret, target_table=dynamo_table, max_retries=3, max_tweets=5)
     
     # Add stream rules to reduce listening events (tweets)
     streamRules = create_stream_rules(config.objects, config.language)
